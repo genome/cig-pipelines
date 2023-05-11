b54b321a7f7fce0e7a0afaf931db73ae3b8f7015
Eddie Belter <ebelter@gmail.com>
2023-05-11 16:09:37 -0500

    Align and Call Pipeline

--- scripts/build_imports.sh
+++ scripts/build_imports.sh
@@ -23,11 +23,15 @@ echo  "${wdl_dn}"
 
 working_dn=$(mktemp -d 2>/dev/null || mktemp -d -t 'build-imports-XXX')
 echo WDL: "${wdl}"
-for import_fn in $(grep import "${wdl}" | awk -F\" '{print $2}' | sed 's#\(..\/\)\+#wdl/#'); do
-    dest_bn=$(dirname ${import_fn})
+for import_wdl in $(grep import "${wdl}" | awk -F\" '{print $2}' | sed 's#\(..\/\)\+#wdl/#'); do
+    dest_bn=$(dirname ${import_wdl})
     dest_dn="${working_dn}/${dest_bn}"
     mkdir -p "${dest_dn}"
-    cp "../${import_fn}" ${dest_dn}
+    cp "../${import_wdl}" ${dest_dn}
+    import_wdl_imports="../${import_wdl/.wdl/.imports.zip}"
+    if [ -e "${import_wdl_imports}" ]; then
+        unzip -qo "${import_wdl_imports}" -d "${working_dn}"
+    fi
 done
 
 prev_dn=$(pwd -P)
